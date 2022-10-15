9de88e6a5d50b5bc7b1f7689867f44d8b387d3ff
nutax <jose.huby@utec.edu.pe>
2022-10-14 20:26:26 -0500

    LAYER: Error function done

--- include/nn/layer.h
+++ include/nn/layer.h
@@ -2,9 +2,9 @@
 #define _NN_LAYER_H_
 
 void nn_layer_forward(const float x[], const float w[], float y[], int inputs, int outputs, void (*act)(float*, int));
-void nn_layer_error(const float r[], const float y[], float d[], int outputs);
+void nn_layer_error(const float r[], const float y[], float e[], int outputs);
 void nn_layer_delta(const float fd[], const float w[], float d[], int inputs, int outputs);
-void nn_layer_update(const float fd[], const float fy[], const float y[], int inputs, int outputs, float rate, void (*dact)(float*, int));
+void nn_layer_update(const float fd[], const float fy[], const float y[], float w[], int inputs, int outputs, float rate, void (*dact)(float*, int));
 
 
 #endif
--- src/nn/layer.c
+++ src/nn/layer.c
@@ -1,13 +1,19 @@
 #include "nn/layer.h"
 
 void nn_layer_forward(const float x[], const float w[], float y[], int inputs, int outputs, void (*act)(float*, int)){
-    int o, h, i;
-    for(o = 0; o<outputs; ++o){
-        y[o] = 0;
-        h = o*inputs;
-        for(i = 0; i<inputs; ++i){
-            y[o] += x[i] * w[h+i];
+    int i, j, h;
+    for(i = 0; i<outputs; ++i){
+        y[i] = 0;
+        h = i*inputs;
+        for(j = 0; j<inputs; ++j){
+            y[i] += x[j] * w[h+j];
         }
     }
     act(y, outputs);
+}
+void nn_layer_error(const float r[], const float y[], float e[], int outputs){
+    int i;
+    for(i = 0; i<outputs; ++i){
+        e[i] = r[i] - y[i];
+    }
 }
