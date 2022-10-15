2b24088bed0b700ec030ff4e702a6ba9b7b20e36
nutax <jose.huby@utec.edu.pe>
2022-10-14 22:20:20 -0500

    LAYER: delta function done

--- include/nn/layer.h
+++ include/nn/layer.h
@@ -2,9 +2,9 @@
 #define _NN_LAYER_H_
 
 void nn_layer_forward(const float x[], const float w[], float y[], int inputs, int outputs, void (*act)(float*, int));
-void nn_layer_error(const float r[], const float y[], float e[], int outputs);
-void nn_layer_delta(const float fd[], const float w[], float d[], int inputs, int outputs);
-void nn_layer_update(const float fd[], const float fy[], const float y[], float w[], int inputs, int outputs, float rate, void (*dact)(float*, int));
+void nn_layer_output_delta(const float ans[], const float y[], float output_d[], int outputs);
+void nn_layer_input_delta(const float d[], const float w[], float input_d[], int inputs, int outputs);
+void nn_layer_update(const float forward_d[], const float forward_y[], const float y[], float w[], int inputs, int outputs, float rate, void (*dact)(float*, int));
 
 
 #endif
--- src/nn/layer.c
+++ src/nn/layer.c
@@ -11,9 +11,22 @@ void nn_layer_forward(const float x[], const float w[], float y[], int inputs, i
     }
     act(y, outputs);
 }
-void nn_layer_error(const float r[], const float y[], float e[], int outputs){
+void nn_layer_output_delta(const float ans[], const float y[], float output_d[], int outputs){
     int i;
     for(i = 0; i<outputs; ++i){
-        e[i] = r[i] - y[i];
+        output_d[i] = ans[i] - y[i];
     }
 }
+void nn_layer_input_delta(const float d[], const float w[], float input_d[], int inputs, int outputs){
+    int i, j, h;
+    for(i = 0; i<inputs; ++i){
+        input_d[i] = 0;
+    }
+    for(i = 0; i<outputs; ++i){
+        h = i*inputs;
+        for(j = 0; j < inputs; ++j){
+            input_d[j] += d[i]*w[h+j];
+        }
+    }
+}
+void
